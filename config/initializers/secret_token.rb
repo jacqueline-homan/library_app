e16aaa8e38225da6a80cee86678ecb3f485d59e2
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-01-11 10:56:28 -0500

    Modified README.rdoc file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibraryApp::Application.config.secret_key_base = 'd29ec0ea28a77103a8d918e599aaff1656b7b152ab24627530c46f416bf4e2d25c9235d5483f65e9edbb096e6b601c48a6d3e3e7d70c49dc64be425aa1ec603b'
